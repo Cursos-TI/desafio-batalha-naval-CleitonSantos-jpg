2e21857ca03aa805c8764f850328515314fc19f9
Cleiton Santos <cleitin.541@gmail.com>
2025-11-03 16:02:05 -0300

    missingOctImpressioin

--- bNmestre.c
+++ bNmestre.c
@@ -16,6 +16,23 @@ void navDiag2 (int tabuleiro[10][10]){
         if ((j>3&&j<7)&&(i>6)){
             tabuleiro [i][j]=3;
         }}}
+/*
+void cone (int i, int j, int centro, int alcance, int limite int tabuleiro [10][10]){
+    for (i=0, limite=3; i< limite; i++){
+        for ((j=(centro=2) - (alcance=0)); j <= centro + alcance; j++){
+        if (i<3 && (j>=0 && j<10)){
+            tabuleiro [i][j]=1;
+        }} alcance++;
+        }}
+
+void cruz (int i, int j, int tabuleiro [10][10]){
+    for (i=2; i<7; i++){
+        for (j=7; j==7; j++){
+            if (j==7 && (i>1 && i<7)){
+                tabuleiro [i][j]=1;
+            }}}}
+
+void octaedro (int i, int j, int tabuleiro [10][10]){} */
 
      //Recursividade do tabuleiro sem marcação.
 void tabBra (int linha, int coluna, int i, int j, int tabuleiro [10][10]){
@@ -62,7 +79,7 @@ void navVHD (int linha, int coluna, int i, int j, int tabuleiro[10][10]){
     }}
 
     //Recursividade do tabuleiro com marcações especiais.
-void navEsp (int linha, int coluna, int i, int j, int tabuleiro [10][10]){
+/*void navEsp (int linha, int coluna, int i, int j, int tabuleiro [10][10]){
     //Impressao da coluna.
     printf ("  ");
     for (coluna='A'; coluna < 'K'; coluna++){
@@ -73,6 +90,9 @@ void navEsp (int linha, int coluna, int i, int j, int tabuleiro [10][10]){
     for (linha='K', i=0; linha < 'U'; linha++, i++){
         printf ("%c ", linha);
         j=0;
+        cone(i, j, centro, alcance, limite, tabuleiro);
+        cruz(i, j, tabuleiro);
+        octaedro(i, j, tabuleiro);
         //Impressão do tabuleiro.
     while (j<10){
         tabuleiro [i][j]=0;
@@ -80,7 +100,7 @@ void navEsp (int linha, int coluna, int i, int j, int tabuleiro [10][10]){
         j++;
     } 
         printf ("\n");
-    }}
+    }} */
 
 int main (){
 
@@ -90,11 +110,18 @@ int main (){
 
     //Menu de seleção do jogo.
     while (opcao1 < 3){
-    printf ("\n\n**BEM VINDO AO JOGO BATALHA NAVAL**\n\n");
+        if (opcao1==0 || opcao1==2){
+    printf ("\n\n***BEM VINDO AO JOGO BATALHA NAVAL***\n\n");
     printf ("1 - COMO O JOGO FUNCIONA.\n");
     printf ("2 - ENTRAR NO JOGO.\n");
     printf ("3 - SAIR DO JOGO.\n\n");
     scanf ("%d", &opcao1);
+    } else if (opcao1==1) {
+        printf ("\n\n***BEM VINDO AO JOGO BATALHA NAVAL***\n\n");
+        printf ("2 - ENTRAR NO JOGO.\n");
+        printf ("3 - SAIR DO JOGO.\n\n");
+        scanf ("%d", &opcao1);
+    }
 
     //Switch de entrado do jogo.
     switch (opcao1){
@@ -115,9 +142,11 @@ int main (){
             printf ("5 - SAIR DO JOGO.\n\n");
             scanf ("%d", &opcao2);
             
+            
+             
+            
     //2º switch para impressão de opções do tabuleiro.
-    switch (opcao2){
-
+        switch (opcao2){
         //Impressão do tabuleiro em branco.
         case (1):
             tabBra(linha, coluna, i, j, tabuleiro);
@@ -130,9 +159,21 @@ int main (){
 
         //Impressão do tabuleiro com caracteres especiais.
         case (3):
-            navEsp(linha, coluna, i, j, tabuleiro);
+           // navEsp(linha, coluna, i, j, tabuleiro);
+           // break;
+        case (4):
             break;
-    }break;
+
+        default:
+        opcao1=3;
+            printf ("...SAINDO...");
+        break;
+        } if (opcao2<5){
+            break;
+        } else {
+            opcao1=3;
+            continue;
+        }
         //Case para saida do jogo.
         default:
             printf ("...SAINDO...");
